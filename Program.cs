bb6cdc30432acd183f4201da76444d44037b9cd7
서예빈 <85054837+cutejay0225@users.noreply.github.com>
2022-04-09 22:27:19 +0900

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 예제_4_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // 배열을 생성
+            int[] intArray = { 52, 273, 32, 65, 103 };
+
+            // 배열의 0번째 요소를 변경
+            intArray[0] = 0;
+
+            // 요소를 출력
+            Console.WriteLine(intArray[0]);
+            Console.WriteLine(intArray[1]);
+            Console.WriteLine(intArray[2]);
+            Console.WriteLine(intArray[3]);
+            Console.WriteLine("컴퓨터공학과, 20210845, 2학년, 홀수반, 서예빈");
+        }
+    }
+}
