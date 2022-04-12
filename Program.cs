e0099f5507dd1a8eb7005845d1b8c227bf845561
서예빈 <85054837+cutejay0225@users.noreply.github.com>
2022-04-13 03:07:10 +0900

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           for(int Line = 0; Line < 10; Line++)
+            {
+                for(int blank = 0; blank<10-Line; blank++)
+                    Console.Write(' ');
+                for(int star = 0; star < 2*Line -1; star++)
+                    Console.Write('*');
+                Console.Write('\n');
+            }
+           for(int Line = 10; Line>0; Line--)
+            {
+                for(int blank = 0; blank<10-Line; blank++)
+                    Console.Write(' ');
+                for(int star = 0; star < 2*Line -1; star++)
+                    Console.Write('*');
+                Console.Write('\n');
+            }
+            
+        }
+
+    }
+
+
+
+}
+
