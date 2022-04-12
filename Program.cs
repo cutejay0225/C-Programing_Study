ea4e3200fa1f35b61bc783b9fe0d90571dcf85a4
서예빈 <85054837+cutejay0225@users.noreply.github.com>
2022-04-12 22:33:19 +0900

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("입력: ");
+            string input = Console.ReadLine();
+
+            if(input.Contains("안녕"))
+            {
+                Console.WriteLine("안녕하세요!");
+            }
+            else
+            {
+                Console.WriteLine("^^");
+            }
+            
+        }
+
+    }
+
+
+
+}
+
