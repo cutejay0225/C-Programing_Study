5544863df380c97a93a32b420a1250102d95655d
서예빈 <85054837+cutejay0225@users.noreply.github.com>
2022-04-12 22:22:23 +0900

    Add files via upload
    switch 조건문

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("이번 달은 몇 월 인가요? : ");
+            int input = int.Parse(Console.ReadLine());
+
+            switch(input)
+            {
+                case 12:
+                case 1:
+                case 2:
+
+                    Console.WriteLine("겨울입니다.");
+                    break;
+
+                case 3:
+                case 4:
+                case 6:
+                    Console.WriteLine("봄입니다.");
+                    break;
+
+                default:
+                    Console.WriteLine("지구가 아닙니다.");
+                    break;
+            }
+            
+        }
+
+    }
+
+
+
+}
+
